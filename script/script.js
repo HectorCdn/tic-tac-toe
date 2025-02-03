8dceae535849d69e7b3ec24234caee3d663b7eb6
HectorCdn <hcottachaudon@edenschool.fr>
2025-02-03 17:36:07 +0100

    SCQSEDSEFC

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const crossDiv = document.querySelector(".cross-div");
+const ovalDiv = document.querySelector(".oval-div");
+
+const cross = document.querySelector(".cross");
+const oval = document.querySelector(".oval");
+
+crossDiv.addEventListener("click", ()  => {
+    crossDiv.classList.add = "selected"
+    cross.classList.remove = "selected" 
+})
+
+ovalDiv.addEventListener("click", ()  => {
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
