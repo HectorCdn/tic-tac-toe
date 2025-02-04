d004c1ff6e5762dde7320f44948e05cde56319be
HectorCdn <hcottachaudon@edenschool.fr>
2025-02-04 13:25:11 +0100

    qzdqzdq

--- script/script.js
+++ script/script.js
@@ -5,8 +5,11 @@ const cross = document.querySelector(".cross");
 const oval = document.querySelector(".oval");
 
 crossDiv.addEventListener("click", ()  => {
-    crossDiv.classList.add = "selected"
-    cross.classList.remove = "selected" 
+    crossDiv.classList.add = "selected";
+    cross.classList.remove = "selected";
+
+    cross.classList.add = "unselected";
+    crossDiv.classList.remove = "selected";
 })
 
 ovalDiv.addEventListener("click", ()  => {
