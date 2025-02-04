ce81b95a00052e5df6aaf833f305d911e42db944
HectorCdn <hcottachaudon@edenschool.fr>
2025-02-04 16:40:15 +0100

    oesijfsf

--- script/script.js
+++ script/script.js
@@ -5,19 +5,24 @@ const cross = document.querySelector(".cross");
 const oval = document.querySelector(".oval");
 
 crossDiv.addEventListener("click", ()  => {
-    crossDiv.classList.add = "selected";
-    cross.classList.remove = "selected";
+    crossDiv.style.backgroundColor = "var(--silver)";
+    cross.style.fill = "var(--dark-navy)";
+    cross.style.backgroundColor = "var(--silver)";
 
-    cross.classList.add = "unselected";
-    crossDiv.classList.remove = "selected";
+    ovalDiv.style.backgroundColor = "var(--dark-navy)";
+    oval.style.fill = "var(--silver)";
+    oval.style.backgroundColor = "var(--dark-navy)";
 })
 
 ovalDiv.addEventListener("click", ()  => {
-    
+    ovalDiv.style.backgroundColor = "var(--silver)";
+    oval.style.fill = "var(--dark-navy)";
+    oval.style.backgroundColor = "var(--silver)";
+
+    crossDiv.style.backgroundColor = "var(--dark-navy)";
+    cross.style.fill = "var(--silver)";
+    cross.style.backgroundColor = "var(--dark-navy)";
 })
-
-
-
 
 
 
