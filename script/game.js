c2e356ceb51baf336c51b4831e5ca7eec401f6f5
HectorCdn <hcottachaudon@edenschool.fr>
2025-02-05 11:38:29 +0100

    dqzdqdqdq

---
+++ script/game.js
@@ -0,0 +1,21 @@
+const player1 = 0;
+const player2 = 1;
+let player;
+
+const button = document.querySelector(".button");
+
+if (player == player1)
+    
+button.addEventListener("click", () => {
+
+})
+
+
+
+
+
+
+
+
+
+
